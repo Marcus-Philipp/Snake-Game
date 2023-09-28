aadd871ed2ee1b559fe4db907122d93aa2348290
Marcus-Philipp <philippmarcus89@gmail.com>
2023-09-28 20:56:59 +0200

    GameLogik mit Bewegung, Richtungen resetGame

--- src/components/GameLogik.js
+++ src/components/GameLogik.js
@@ -3,83 +3,125 @@ import GameBoard from './GameBoard';
 
 const GameLogik = () => {
     const initialSnakeBody = [
-        { x: 10, y: 10 }, //Kopf
-        { x: 10, y: 9 }, //Korper
-        { x: 10, y: 8 } //Schwanz
+        { x: 10, y: 5 }, //Kopf
+        { x: 10, y: 4 }, //Koerper
+        { x: 10, y: 3 } //Schwanz
     ];
 
-    const initialFood = {
+    const generateFood = () => ({
+
         x: Math.floor(Math.random() * 20),
         y: Math.floor(Math.random() * 20)
-    }
+    });
 
     const [snake, setSnake] = useState(initialSnakeBody);
 
-    const [food, setFood] = useState(initialFood);
+    const [food, setFood] = useState(generateFood);
 
-    const [direction, setDirection] = useState('RIGHT');
+    const [direction, setDirection] = useState('DOWN');
+
+    const moveSnake = (currentSnake, currentDirection, shouldGrow = false) => {
+        let newSnake = [...currentSnake];
+
+        let head = { ...newSnake[0] };
+
+        switch (currentDirection) {
+            case 'RIGHT':
+                head.x += 1;
+                break;
+            case 'LEFT':
+                head.x -= 1;
+                break;
+            case 'UP':
+                head.y -= 1;
+                break;
+            case 'DOWN':
+                head.y += 1;
+                break;
+            default:
+                break;
+        }
+
+        newSnake.unshift(head);
+        if (!shouldGrow) {
+            newSnake.pop();
+        };
+
+        return newSnake;
+
+    };
+
+    const resetGame = () => {
+        setSnake(initialSnakeBody);
+        setFood(generateFood());
+        setDirection('DOWN');
+    };
+
+    const isCollisionWithSelf = (head, snakeBody) => {
+
+        for (let segment of snakeBody.slice(1)) {
+            if (segment.x === head.x && segment.y === head.y) {
+                return true;
+            }
+        }
+
+        return false;
+    };
 
     useEffect(() => {
-        const moveSnack = () => {
-            let newSnake = [...snake];
 
-            let head = Object.assign({}, newSnake[0]);
+        const move = () => {
+            const newSnake = moveSnake(snake, direction);
 
-            switch (direction) {
-                case 'RIGHT':
-                    head.x += 1;
-                    break;
-                case 'LEFT':
-                    head.x -= 1;
-                    break;
-                case 'UP':
-                    head.y -= 1;
-                    break;
-                case 'DOWN':
-                    head.y += 1;
-                    break;
-                default:
-                    break;
-            }
+            const head = newSnake[0];
 
-            newSnake.unshift(head);
-            newSnake.pop();
+            if (head.x === food.x && head.y === food.y) {
+                newSnake.unshift(head);
+                setFood(generateFood());
+            };
+
+            if (isCollisionWithSelf(head, newSnake)) {
+                resetGame();
+                return;
+            };
 
             setSnake(newSnake);
 
         };
 
-        const gameInterval = setInterval(moveSnack, 500);
+        const gameInterval = setInterval(move, 100);
 
         return () => clearInterval(gameInterval);
-    }, [snake, direction]);
+    }, [snake, direction, food.x, food.y, resetGame]);
+
+
+    const handleKeyDown = (e) => {
+
+        switch (e.key) {
+            case 'ArrowRight':
+                setDirection('RIGHT');
+                break;
+            case 'ArrowLeft':
+                setDirection('LEFT');
+                break;
+            case 'ArrowUp':
+                setDirection('UP');
+                break;
+            case 'ArrowDown':
+                setDirection('DOWN');
+                break;
+            default:
+                break;
+        }
+    };
 
     useEffect(() => {
-        const handleKeyDown = (e) => {
 
-            switch (e.key) {
-                case 'ArrowRight':
-                    if (direction === 'RIGHT') setDirection('RIGHT');
-                    break;
-                case 'ArrowLeft':
-                    if (direction === 'LEFT') setDirection('LEFT');
-                    break;
-                case 'ArrowUp':
-                    if (direction === 'UP');
-                    break;
-                case 'ArrowDown':
-                    if (direction === 'DOWN');
-                    break;
-                default:
-                    break;
-            }
-          };
-
-          window.addEventListener('keydown', handleKeyDown);
-          return () => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
             window.removeEventListener('keydown', handleKeyDown);
-        } 
-    }, [direction]);
+        };
+    }, []);
 
     return <GameBoard snakeBody={snake} food={food} />
 };
